ffa11e5a58142db05be1d8c31a1fb3891bd67078
Adelin Bojan <adelinbojan96@gmail.com>
2024-04-07 13:44:25 +0300

    frontend first version

--- src/main/java/GUI/SimulationFrame.java
+++ src/main/java/GUI/SimulationFrame.java
@@ -1,7 +1,39 @@
 package GUI;
 
-public class SimulationFrame {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class SimulationFrame extends JDialog{
+    private JPanel mainPanel;
+    private JTextArea numberClientsTextArea;
+    private JButton startButton;
+    private JLabel image;
+
+    public SimulationFrame()
+    {
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setSize(650, 460);
+        setTitle("Enter inputs for the operations");
+        ImageIcon icon = new ImageIcon("src/pictures/shop.png");
+        image.setIcon(icon);
+        //customizeButton(startButton);
+        setContentPane(mainPanel);
+        setModal(true);
+        setVisible(true);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //access the gui_frame
+        new SimulationFrame();
+    }
+    private static void customizeButton(JButton button)
+    {
+        button.setBackground(Color.WHITE);
+        button.setForeground(Color.BLACK);
+        button.setBorder(BorderFactory.createCompoundBorder(
+                new LineBorder(Color.BLACK, 2, true),
+                new EmptyBorder(5, 20, 5, 20)
+        ));
     }
 }
