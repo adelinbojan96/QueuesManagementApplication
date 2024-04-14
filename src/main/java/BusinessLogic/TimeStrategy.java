075ed95d7c9c0e10bcca906c08274f0b02fa901b
Adelin Bojan <adelinbojan96@gmail.com>
2024-04-14 09:55:43 +0300

    implementation of shortest q and time q

--- src/main/java/BusinessLogic/TimeStrategy.java
+++ src/main/java/BusinessLogic/TimeStrategy.java
@@ -1,27 +1,2 @@
-package BusinessLogic;
-
-import Model.Server;
-import Model.Task;
-
-import java.util.List;
-
-public class TimeStrategy implements Strategy{
-    @Override
-    public void addTask(List<Server> servers, Task task) {
-        if(!servers.isEmpty())
-        {
-            int smallestTime = servers.get(0).getWaitingPeriod().get();
-            int index = 0;
-            for(int i = 1; i < servers.size(); i++)
-            {
-                int waitingPeriodCurrentServer = servers.get(i).getWaitingPeriod().get();
-                if(waitingPeriodCurrentServer < smallestTime)
-                {
-                    smallestTime = waitingPeriodCurrentServer;
-                    index = i;
-                }
-            }
-            servers.get(index).addTask(task);
-        }
-    }
+package BusinessLogic;public class TimeStrategy {
 }
